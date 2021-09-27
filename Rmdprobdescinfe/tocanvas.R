d13c3225b55ea56f9ed45fbc5959efce0c6a630a
Eva Freyhult <eva@freyhult.net>
2021-09-27 14:23:52 +0200

    Update and reformat descriptive, probability, inference, clustering

---
+++ Rmdprobdescinfe/tocanvas.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+sections <- data.frame(x=system2("grep", "'^# ' *.Rmd | grep -v '{-}'", stdout=TRUE))
+
+cap <- function(x) paste0(toupper(substr(x, 1,1)), substr(x,2,str_length(x)))
+sections <- sections %>% mutate(session = gsub("[0-9]*", "", gsub(".Rmd.*", "", x)),
+                                Session=cap(session),
+                                name = gsub(".*# ", "", gsub(" [{].*", "", x)),
+                                id = gsub(".*#", "", gsub(".*[{]", "", gsub("[}]", "", x))))
+
+
+tocanvas <- function(session, Session, name, id) {
+  write(sprintf("<p><iframe style=\"overflow: hidden;\" src=\"https://nbisweden.github.io/workshop-mlbiostatistics/session-%s/%s.html\" width=\"1000\" height=\"800\"></iframe></p>", session, id), file="tmp.html")
+  system2("curl", sprintf("-X POST --header \"Authorization: Bearer 14589~69wK1p5Q9Q8pcn2WFfl7NL5hwbZQdAhFn6V0Bzwiytap4RagAAHZziZ7biWXrbWk\" https://uppsala.instructure.com/api/v1/courses/51998/pages --data-urlencode wiki_page[title]=\"%s: %s\" --data-urlencode wiki_page[body]@tmp.html", Session, name))
+}
+
+apply(sections[1:4,], 1, tocanvas)
