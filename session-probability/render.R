4255e37cccbb27969ca0f2f5e14e33ee5defa5fd
evaf <eva@freyhult.net>
2022-08-31 10:37:57 +0200

    Move Rmd files to their respective session-* directory

---
+++ session-probability/render.R
@@ -0,0 +1,19 @@
+##Exercise files, remove solutions
+system("for x in *_solutions.Rmd; do base=${x%%_solutions.*}; awk 'BEGIN {flag=1} /*Solution*/{flag=0;next}/End solution/{flag=1;next}flag' $x > ${base}.Rmd; done")
+##Render exercise files with solutions separately
+tmp <- lapply(list.files(pattern="solutions.Rmd"), function(rmd) rmarkdown::render(rmd))
+
+##Render book for canvas
+bookdown::render_book(c("index.Rmd", "probability.Rmd"), output_dir=".", preview=TRUE, bookdown::html_book(lib_dir = "libs", number_sections=TRUE, toc=FALSE, split_by="chapter"))
+
+##Upload to canvas
+#kursid=70535
+#token generated from settings in canvas
+source("tocanvas.R")
+wd <- getwd()
+setwd("..")
+sapply(list.files("session-probability", "prob-.*html", full.names = TRUE), postpage, token=token, kursid=kursid)
+
+setwd(wd)
+##Render book for github
+bookdown::render_book(c("index.Rmd", "probability.Rmd"), output_dir=".", preview=TRUE, bookdown::gitbook(lib_dir = "libs"))
