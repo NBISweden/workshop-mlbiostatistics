66380e3577e7808754c7f703be2bf49fec76c374
Mun-Gwan Hong <mungwan.hong@nbis.se>
2022-09-11 22:18:50 +0200

    Clustering - HTML images from github.io

---
+++ session-clustering/html/adjust_html_for_canvas.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+COURSE_GITHUB_IO="https://nbisweden.github.io/workshop-mlbiostatistics/session-clustering/html"
+
+for html_out in chapters exercises exercises-solutions
+do
+  html_out_files=$html_out"_files"
+  sed -e "s,$html_out_files/figure-html,$COURSE_GITHUB_IO/$html_out_files/figure-html,g" $html_out.html > to_canvas/$html_out.html
+done
