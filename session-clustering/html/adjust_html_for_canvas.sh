9985a26bbefe7e0113813edf21b49538d35d986e
Mun-Gwan Hong <mungwan.hong@nbis.se>
2022-09-11 22:25:42 +0200

    fix adjust_html_for_canvas.sh to include images/

--- session-clustering/html/adjust_html_for_canvas.sh
+++ session-clustering/html/adjust_html_for_canvas.sh
@@ -4,5 +4,6 @@ COURSE_GITHUB_IO="https://nbisweden.github.io/workshop-mlbiostatistics/session-c
 for html_out in chapters exercises exercises-solutions
 do
   html_out_files=$html_out"_files"
-  sed -e "s,$html_out_files/figure-html,$COURSE_GITHUB_IO/$html_out_files/figure-html,g" $html_out.html > to_canvas/$html_out.html
+  sed -e "s,$html_out_files/figure-html,$COURSE_GITHUB_IO/$html_out_files/figure-html,g" $html_out.html |
+    sed -e "s,src=\"images/,src=\"$COURSE_GITHUB_IO/images/,g" > to_canvas/$html_out.html
 done
